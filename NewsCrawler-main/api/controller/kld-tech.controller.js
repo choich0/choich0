fe0a4bbdd79dc20e839a82d80ce1e431debe3329
choich0 <98073263+choich0@users.noreply.github.com>
2022-11-25 14:29:58 +0900

    Create kld-tech.controller.js

---
+++ NewsCrawler-main/api/controller/kld-tech.controller.js
@@ -0,0 +1,48 @@
+const { KLDTECH } = require('../database');
+
+async function getAll(req, res){
+    const result = await KLDTECH.findAll();
+    res.status(200).json({ result });
+}
+
+async function update(req, res){
+    const { nTitle, nDate, nLink } = req.body;
+    if(!nTitle || !nDate || !nLink){
+        res.status(400).json({ error: 'nTitle, nDate and nLink are required'});
+        return;
+    }
+
+    const count = await KLDTECH.count({ where: { nTitle, nDate, nLink }});
+
+    if(count === 0) {
+        await KLDTECH.create(req.body);
+    } else {
+        await KLDTECH.update(req.body, { where: { nTitle, nDate, nLink }});
+    }
+
+    res.status(200).json({ result: 'success' });
+}
+
+async function remove(req, res) {
+    const { nTitle, nDate, nLink } = req.body;
+    if(!nTitle || !nDate || !nLink){
+        res.status(400).json({ error: 'nTitle, nDate and nLink are required'});
+        return;
+    }
+
+    await KLDTECH.destroy({
+        where: {
+            nTitle,
+            nDate,
+            nLink,          
+        },
+    });
+
+    res.status(200).json({ result: 'success' });
+}
+
+module.exports = {
+    getAll,
+    update,
+    remove,
+};
