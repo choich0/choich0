6520d03e9adb34bb9bb9c6595e732cd08664f71b
choich0 <98073263+choich0@users.noreply.github.com>
2022-11-25 14:49:17 +0900

    Create dnews-mat.controller.js

---
+++ api/controller/dnews-mat.controller.js
@@ -0,0 +1,48 @@
+const { DNEWSMAT } = require('../database');
+
+async function getAll(req, res){
+    const result = await DNEWSMAT.findAll();
+    res.status(200).json({ result });
+}
+
+async function update(req, res){
+    const { nTitle, nDate, nLink } = req.body;
+    if(!nTitle || !nDate || !nLink){
+        res.status(400).json({ error: 'nTitle, nDate and nLink are required'});
+        return;
+    }
+
+    const count = await DNEWSMAT.count({ where: { nTitle, nDate, nLink }});
+
+    if(count === 0) {
+        await DNEWSMAT.create(req.body);
+    } else {
+        await DNEWSMAT.update(req.body, { where: { nTitle, nDate, nLink }});
+    }
+
+    res.status(200).json({ result: 'success' });
+}
+
+async function remove(req, res) {
+    const { nTitle, nDate, nLink } = req.body;
+    if(!nTitle || !nDate || !nLink){
+        res.status(400).json({ error: 'nTitle, nDate and nLink are required'});
+        return;
+    }
+
+    await DNEWSMAT.destroy({
+        where: {
+            nTitle,
+            nDate,
+            nLink,          
+        },
+    });
+
+    res.status(200).json({ result: 'success' });
+}
+
+module.exports = {
+    getAll,
+    update,
+    remove,
+};
