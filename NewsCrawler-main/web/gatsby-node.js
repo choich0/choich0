31c4ef894ee99abe9db2a5623bc0552d0bf2943d
choich0 <98073263+choich0@users.noreply.github.com>
2022-11-25 14:33:05 +0900

    Update gatsby-node.js

--- NewsCrawler-main/web/gatsby-node.js
+++ NewsCrawler-main/web/gatsby-node.js
@@ -1,12 +1,30 @@
-const { getDataSource } = require("./src/data-loader");
+const { getKld } = require("./src/kld-data-loader");
+const { getDnews } = require("./src/dnews-data-loader");
+const { getMolit } = require("./src/molit-data-loader");
 
 exports.createPages = async ({ actions }) => {
   const { createPage } = actions;
-  const dataSource = await getDataSource();
+  var dataSource = await getKld();
 
   createPage({
     path: "/",
-    component: require.resolve("./src/templates/single-page.js"),
+    component: require.resolve("./src/templates/kld-single-page.js"),
+    context: { dataSource },
+  });
+
+  var dataSource = await getDnews();
+
+  createPage({
+    path: "/dnews",
+    component: require.resolve("./src/templates/dnews-single-page.js"),
+    context: { dataSource },
+  });
+
+  var dataSource = await getMolit();
+
+  createPage({
+    path: "/molit",
+    component: require.resolve("./src/templates/molit-single-page.js"),
     context: { dataSource },
   });
 };
