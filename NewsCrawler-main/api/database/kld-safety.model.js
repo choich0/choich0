7862466b864c95f088ec1e3aa6eaa7f74c6d976e
choich0 <98073263+choich0@users.noreply.github.com>
2022-11-25 14:30:38 +0900

    Create kld-safety.model.js

---
+++ NewsCrawler-main/api/database/kld-safety.model.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  return sequelize.define(
+      "KLDSAFETY",
+      {
+        id: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER.UNSIGNED,
+          allowNull: false,
+          primaryKey: true,
+        },
+        nTitle: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        nDate: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+        },
+        nLink: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        timestamps: false,
+      }
+    );
+};
