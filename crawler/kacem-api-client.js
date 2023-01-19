149db1aef63a1c284fd04407b3f118fb5b03e9f2
choich0 <98073263+choich0@users.noreply.github.com>
2023-01-19 14:16:56 +0900

    Create kacem-api-client.js

---
+++ crawler/kacem-api-client.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+
+class ApiClientKacemAnn {
+  constructor() {
+    // HTTP 클라이언트
+    const client = axios.create({
+      baseURL: process.env.CB_API_BASE_URL || "http://localhost:8080",
+    });
+
+    // 응답에 대한 인터셉터를 등록하면 모든 응답에 대해 여기 등록된 함수 수행
+    client.interceptors.response.use((resp) => {
+      return resp.data;
+    });
+
+    this.client = client;
+  }
+
+  // kacemAnn update API 호출
+  async updateKacemAnn(data) {
+    return await this.client.post("kacemAnn", data);
+  }
+}
+
+module.exports = { ApiClientKacemAnn };
