68d3b31cb84c1b3006b7dd7c0bbd3248487864ca
choich0 <98073263+choich0@users.noreply.github.com>
2023-01-19 14:17:07 +0900

    Create kcnet-updater.js

---
+++ crawler/kcnet-updater.js
@@ -0,0 +1,387 @@
+const _ = require("lodash");
+const fs = require("fs");
+const path = require("path");
+const { KcnetTechCrawling, KcnetNotCrawling, KcnetNewsCrawling } = require("./kcnet-crawler");
+
+async function kcnetTechUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "kcnetTech.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous kcnetTech not found");
+  }
+
+  const newsCralwer = await KcnetTechCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("kcnetTech has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("kcnetTech updated successfully");
+}
+
+async function kcnetNotUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "kcnetNot.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous kcnetNot not found");
+  }
+
+  const newsCralwer = await KcnetNotCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("kcnetNot has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("kcnetNot updated successfully");
+}
+
+async function kcnetNewsUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "kcnetNews.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous kcnetNews not found");
+  }
+
+  const newsCralwer = await KcnetNewsCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("kcnetNews has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("kcnetNews updated successfully");
+}
+
+module.exports = { kcnetTechUpdate, kcnetNotUpdate, kcnetNewsUpdate };
