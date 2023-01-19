e85784335a51ee22ae0912c86eb941fe1c7088ca
choich0 <98073263+choich0@users.noreply.github.com>
2023-01-19 14:15:26 +0900

    Create rocm-s.model.js

---
+++ api/database/rocm-s.model.js
@@ -0,0 +1,30 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  return sequelize.define(
+      "SROCM",
+      {
+        id: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER.UNSIGNED,
+          allowNull: false,
+          primaryKey: true,
+        },
+        nTitle: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+        nDate: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+        },
+        nLink: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+      },
+      {
+        timestamps: false,
+      }
+    );
+};
