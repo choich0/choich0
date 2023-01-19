952cfc98361505507c25db75112b6f5628d15f81
choich0 <98073263+choich0@users.noreply.github.com>
2023-01-19 14:21:15 +0900

    Create work-api-client.js

---
+++ crawler/work-api-client.js
@@ -0,0 +1,66 @@
+const axios = require("axios");
+
+class ApiClientWorkRaw {
+  constructor() {
+    // HTTP 클라이언트
+    const client = axios.create({
+      baseURL: process.env.CB_API_BASE_URL || "http://localhost:8080",
+    });
+
+    // 응답에 대한 인터셉터를 등록하면 모든 응답에 대해 여기 등록된 함수 수행
+    client.interceptors.response.use((resp) => {
+      return resp.data;
+    });
+
+    this.client = client;
+  }
+
+  // workRaw update API 호출
+  async updateWorkRaw(data) {
+    return await this.client.post("workRaw", data);
+  }
+}
+
+class ApiClientWorkPoly {
+  constructor() {
+    // HTTP 클라이언트
+    const client = axios.create({
+      baseURL: process.env.CB_API_BASE_URL || "http://localhost:8080",
+    });
+
+    // 응답에 대한 인터셉터를 등록하면 모든 응답에 대해 여기 등록된 함수 수행
+    client.interceptors.response.use((resp) => {
+      return resp.data;
+    });
+
+    this.client = client;
+  }
+
+  // workPoly update API 호출
+  async updateWorkPoly(data) {
+    return await this.client.post("workPoly", data);
+  }
+}
+
+class ApiClientWorkJud {
+  constructor() {
+    // HTTP 클라이언트
+    const client = axios.create({
+      baseURL: process.env.CB_API_BASE_URL || "http://localhost:8080",
+    });
+
+    // 응답에 대한 인터셉터를 등록하면 모든 응답에 대해 여기 등록된 함수 수행
+    client.interceptors.response.use((resp) => {
+      return resp.data;
+    });
+
+    this.client = client;
+  }
+
+  // workJud update API 호출
+  async updateWorkJud(data) {
+    return await this.client.post("workJud", data);
+  }
+}
+
+module.exports = { ApiClientWorkRaw, ApiClientWorkPoly, ApiClientWorkJud };
