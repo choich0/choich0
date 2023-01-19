ad6946f5cbe221c85f53890f06dc33a0fbf9eb7a
choich0 <98073263+choich0@users.noreply.github.com>
2023-01-19 14:20:14 +0900

    Create work-updater.js

---
+++ crawler/work-updater.js
@@ -0,0 +1,387 @@
+const _ = require("lodash");
+const fs = require("fs");
+const path = require("path");
+const { WorkRawCrawling, WorkPolyCrawling, WorkJudCrawling } = require("./work-crawler");
+
+async function workRawUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "workRaw.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous workRaw not found");
+  }
+
+  const newsCralwer = await WorkRawCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("workRaw has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateWorkRaw({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("workRaw updated successfully");
+}
+
+async function workPolyUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "workPoly.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous workPoly not found");
+  }
+
+  const newsCralwer = await WorkPolyCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("workPoly has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateWorkPoly({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("workPoly updated successfully");
+}
+
+async function workJudUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "workJud.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous workJud not found");
+  }
+
+  const newsCralwer = await WorkJudCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("workJud has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateWorkJud({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("workJud updated successfully");
+}
+
+module.exports = { workRawUpdate, workPolyUpdate, workJudUpdate };
