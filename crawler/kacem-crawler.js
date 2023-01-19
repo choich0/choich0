b51e0d1b8c2cf6ef61508b9bf5c58bab0ce44c73
choich0 <98073263+choich0@users.noreply.github.com>
2023-01-19 14:16:58 +0900

    Create kacem-crawler.js

---
+++ crawler/kacem-crawler.js
@@ -0,0 +1,44 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+const iconv = require('iconv-lite');
+
+const getKacemAnn = async () => {
+    try {
+        const kacemAnnHtml = (
+            await axios.get("https://www.ekacem.or.kr:446/news/announce_li.asp", {responseType: "arraybuffer"})
+        );
+        return kacemAnnHtml;
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const KacemAnnCrawling = async() => {
+    process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0; //nodejs 에서 https 접속시 오류 해결
+    const kacemAnnHtml = await getKacemAnn();
+    const content = iconv.decode(kacemAnnHtml.data, "EUC-KR").toString() //한글 인코딩
+    const $ = cheerio.load(content);
+
+    let news = [];
+    for (var i = 2; i < 12; i++) {
+        const list_text_inner_arr = $(
+            "#contents > div.mainConTB > div.contentsView > div > table.bbs_list > tbody > tr:nth-child(" + i + ")"
+        ).toArray();              
+        
+        list_text_inner_arr.forEach((li) => {
+            const title = $(li).find(".lt"); 
+            const path = $(li).find("a").first().attr("href");
+            const dateStr = $(li).find("td:nth-child(4)");
+            const date = dateStr.text().replace(/[^0-9, -]/g, "")
+
+            news.push({
+                nTitle: title.text().trim(),
+                nDate: date.trim().substring(0, 10),
+                nLink: `https://www.ekacem.or.kr:446${path}`
+            });
+        });
+    };
+    return news;
+}
+
+module.exports = { KacemAnnCrawling };
