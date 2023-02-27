11785e47230a07d04f582693cb02126f362b31c1
choich0 <98073263+choich0@users.noreply.github.com>
2023-02-27 10:37:11 +0900

    Create policy-updater.js

---
+++ crawler/policy-updater.js
@@ -0,0 +1,514 @@
+const _ = require("lodash");
+const fs = require("fs");
+const path = require("path");
+const { PolicyDhCrawling, PolicyLeCrawling, PolicyConCrawling, PolicyMcCrawling } = require("./policy-crawler");
+
+async function policyDhUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "policyDh.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous policyDh not found");
+  }
+
+  const newsCralwer = await PolicyDhCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("policyDh has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updatePolicyDh({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("policyDh updated successfully");
+}
+
+async function policyLeUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "policyLe.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous policyLe not found");
+  }
+
+  const newsCralwer = await PolicyLeCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("policyLe has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updatePolicyLe({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("policyLe updated successfully");
+}
+
+async function policyConUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "policyCon.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous policyCon not found");
+  }
+
+  const newsCralwer = await PolicyConCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("policyCon has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updatePolicyCon({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("policyCon updated successfully");
+}
+
+async function policyMcUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "policyMc.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous policyMc not found");
+  }
+
+  const newsCralwer = await PolicyMcCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("policyMc has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updatePolicyMc({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log("policyMc updated successfully");
+}
+
+module.exports = { policyDhUpdate, policyLeUpdate, policyConUpdate, policyMcUpdate };
