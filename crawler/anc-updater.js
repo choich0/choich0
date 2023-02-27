43fc9d7341e2853c13c5f443bfb1c508455228f9
choich0 <98073263+choich0@users.noreply.github.com>
2023-02-27 10:32:24 +0900

    Create anc-updater.js

---
+++ crawler/anc-updater.js
@@ -0,0 +1,514 @@
+const _ = require("lodash");
+const fs = require("fs");
+const path = require("path");
+const { AncCmCrawling, AncSafetyCrawling, AncPolCrawling, AncGreCrawling } = require("./anc-crawler");
+
+async function ancCmUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "ancCm.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous ancCm not found");
+  }
+
+  const newsCralwer = await AncCmCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("ancCm has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateAncCm({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log('ancCm updated successfully')
+}
+
+async function ancSafetyUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "ancSafety.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous ancSafety not found");
+  }
+
+  const newsCralwer = await AncSafetyCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("ancSafety has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateAncSafety({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log('ancSafety updated successfully')
+}
+
+async function ancPolUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "ancPol.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous ancPol not found");
+  }
+
+  const newsCralwer = await AncPolCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("ancPol has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateAncPol({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log('ancPol updated successfully')
+}
+
+async function ancGreUpdate(outputPath, apiClient) {
+  let prevData = {};
+  const newsListPath = path.join(outputPath, "ancGre.json");
+  try {
+    // 기존 크롤링한 값이 있다면 불러오기
+    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
+  } catch (e) {
+    console.log("previous ancGre not found");
+  }
+
+  const newsCralwer = await AncGreCrawling();
+
+  const newData = {
+    newsList1: newsCralwer[9],
+    newsList2: newsCralwer[8],
+    newsList3: newsCralwer[7],
+    newsList4: newsCralwer[6],
+    newsList5: newsCralwer[5],
+    newsList6: newsCralwer[4],
+    newsList7: newsCralwer[3],
+    newsList8: newsCralwer[2],
+    newsList9: newsCralwer[1],
+    newsList10: newsCralwer[0],
+  };
+
+  // 변경된 값이 없으면 아무 것도 하지 않음
+  if (_.isEqual(newData, prevData)) {
+    console.log("ancGre has not been changed");
+    return;
+  }
+
+  // 크롤링 된 최신 값을 파일에 저장해 둠
+  fs.writeFileSync(newsListPath, JSON.stringify(newData));
+
+  var newNewsList = newData.newsList1;
+  var { nTitle, nDate, nLink } = newNewsList;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList2 = newData.newsList2;
+  var { nTitle, nDate, nLink } = newNewsList2;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList3 = newData.newsList3;
+  var { nTitle, nDate, nLink } = newNewsList3;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList4 = newData.newsList4;
+  var { nTitle, nDate, nLink } = newNewsList4;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList5 = newData.newsList5;
+  var { nTitle, nDate, nLink } = newNewsList5;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList6 = newData.newsList6;
+  var { nTitle, nDate, nLink } = newNewsList6;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList7 = newData.newsList7;
+  var { nTitle, nDate, nLink } = newNewsList7;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList8 = newData.newsList8;
+  var { nTitle, nDate, nLink } = newNewsList8;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList9 = newData.newsList9;
+  var { nTitle, nDate, nLink } = newNewsList9;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList10 = newData.newsList10;
+  var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateAncGre({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  console.log('ancGre updated successfully')
+}
+
+module.exports = { ancCmUpdate, ancSafetyUpdate, ancPolUpdate, ancGreUpdate };
