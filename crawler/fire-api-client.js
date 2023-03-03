32a8e1fed4a27960f4952592a9b22d18a1c6aef9
choich0 <98073263+choich0@users.noreply.github.com>
2023-03-03 14:19:03 +0900

    Update fire-api-client.js

--- crawler/fire-api-client.js
+++ crawler/fire-api-client.js
@@ -42,27 +42,6 @@ class ApiClientFireNfa {
   }
 }
 
-class ApiClientFireLaw {
-  constructor() {
-    // HTTP 클라이언트
-    const client = axios.create({
-      baseURL: process.env.CB_API_BASE_URL || "http://localhost:8080",
-    });
-
-    // 응답에 대한 인터셉터를 등록하면 모든 응답에 대해 여기 등록된 함수 수행
-    client.interceptors.response.use((resp) => {
-      return resp.data;
-    });
-
-    this.client = client;
-  }
-
-  // fireLaw update API 호출
-  async updateFireLaw(data) {
-    return await this.client.post("fireLaw", data);
-  }
-}
-
 class ApiClientFireInfo {
   constructor() {
     // HTTP 클라이언트
@@ -84,4 +63,4 @@ class ApiClientFireInfo {
   }
 }
 
-module.exports = { ApiClientFireFpn, ApiClientFireNfa, ApiClientFireLaw, ApiClientFireInfo };
+module.exports = { ApiClientFireFpn, ApiClientFireNfa, ApiClientFireInfo };
