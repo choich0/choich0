3c12159c912ac4076379ff3a0b217a22406aa60e
choich0 <98073263+choich0@users.noreply.github.com>
2023-03-03 14:19:24 +0900

    Update fire-updater.js

--- crawler/fire-updater.js
+++ crawler/fire-updater.js
@@ -1,7 +1,7 @@
 const _ = require("lodash");
 const fs = require("fs");
 const path = require("path");
-const { FireFpnCrawling, FireNfaCrawling, FireLawCrawling, FireInfoCrawling } = require("./fire-crawler");
+const { FireFpnCrawling, FireNfaCrawling, FireInfoCrawling } = require("./fire-crawler");
 
 async function fireFpnUpdate(outputPath, apiClient) {
   let prevData = {};
@@ -257,133 +257,6 @@ async function fireNfaUpdate(outputPath, apiClient) {
   console.log('fireNfa updated successfully')
 }
 
-async function fireLawUpdate(outputPath, apiClient) {
-  let prevData = {};
-  const newsListPath = path.join(outputPath, "fireLaw.json");
-  try {
-    // 기존 크롤링한 값이 있다면 불러오기
-    prevData = JSON.parse(fs.readFileSync(newsListPath, "utf-8"));
-  } catch (e) {
-    console.log("previous fireLaw not found");
-  }
-
-  const newsCralwer = await FireLawCrawling();
-
-  const newData = {
-    newsList1: newsCralwer[9],
-    newsList2: newsCralwer[8],
-    newsList3: newsCralwer[7],
-    newsList4: newsCralwer[6],
-    newsList5: newsCralwer[5],
-    newsList6: newsCralwer[4],
-    newsList7: newsCralwer[3],
-    newsList8: newsCralwer[2],
-    newsList9: newsCralwer[1],
-    newsList10: newsCralwer[0],
-  };
-
-  // 변경된 값이 없으면 아무 것도 하지 않음
-  if (_.isEqual(newData, prevData)) {
-    console.log("fireLaw has not been changed");
-    return;
-  }
-
-  // 크롤링 된 최신 값을 파일에 저장해 둠
-  fs.writeFileSync(newsListPath, JSON.stringify(newData));
-
-  var newNewsList = newData.newsList1;
-  var { nTitle, nDate, nLink } = newNewsList;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList2 = newData.newsList2;
-  var { nTitle, nDate, nLink } = newNewsList2;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList3 = newData.newsList3;
-  var { nTitle, nDate, nLink } = newNewsList3;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList4 = newData.newsList4;
-  var { nTitle, nDate, nLink } = newNewsList4;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList5 = newData.newsList5;
-  var { nTitle, nDate, nLink } = newNewsList5;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList6 = newData.newsList6;
-  var { nTitle, nDate, nLink } = newNewsList6;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList7 = newData.newsList7;
-  var { nTitle, nDate, nLink } = newNewsList7;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList8 = newData.newsList8;
-  var { nTitle, nDate, nLink } = newNewsList8;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList9 = newData.newsList9;
-  var { nTitle, nDate, nLink } = newNewsList9;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  var newNewsList10 = newData.newsList10;
-  var { nTitle, nDate, nLink } = newNewsList10;
-
-  await apiClient.updateFireLaw({
-    nTitle,
-    nDate,
-    nLink,
-  });
-
-  console.log('fireLaw updated successfully')
-}
-
 async function fireInfoUpdate(outputPath, apiClient) {
   let prevData = {};
   const newsListPath = path.join(outputPath, "fireInfo.json");
@@ -511,4 +384,4 @@ async function fireInfoUpdate(outputPath, apiClient) {
   console.log('fireInfo updated successfully')
 }
 
-module.exports = { fireFpnUpdate, fireNfaUpdate, fireLawUpdate, fireInfoUpdate };
+module.exports = { fireFpnUpdate, fireNfaUpdate, fireInfoUpdate };
