23e8430b699f2499ea56ef19b03bc30676419d1a
choich0 <98073263+choich0@users.noreply.github.com>
2023-03-30 12:20:41 +0900

    Update kcnet-updater.js

--- crawler/kcnet-updater.js
+++ crawler/kcnet-updater.js
@@ -16,16 +16,26 @@ async function kcnetTechUpdate(outputPath, apiClient) {
   const newsCralwer = await KcnetTechCrawling();
 
   const newData = {
-    newsList1: newsCralwer[9],
-    newsList2: newsCralwer[8],
-    newsList3: newsCralwer[7],
-    newsList4: newsCralwer[6],
-    newsList5: newsCralwer[5],
-    newsList6: newsCralwer[4],
-    newsList7: newsCralwer[3],
-    newsList8: newsCralwer[2],
-    newsList9: newsCralwer[1],
-    newsList10: newsCralwer[0],
+    newsList1: newsCralwer[19],
+    newsList2: newsCralwer[18],
+    newsList3: newsCralwer[17],
+    newsList4: newsCralwer[16],
+    newsList5: newsCralwer[15],
+    newsList6: newsCralwer[14],
+    newsList7: newsCralwer[13],
+    newsList8: newsCralwer[12],
+    newsList9: newsCralwer[11],
+    newsList10: newsCralwer[10],
+    newsList11: newsCralwer[9],
+    newsList12: newsCralwer[8],
+    newsList13: newsCralwer[7],
+    newsList14: newsCralwer[6],
+    newsList15: newsCralwer[5],
+    newsList16: newsCralwer[4],
+    newsList17: newsCralwer[3],
+    newsList18: newsCralwer[2],
+    newsList19: newsCralwer[1],
+    newsList20: newsCralwer[0],
   };
 
   // 변경된 값이 없으면 아무 것도 하지 않음
@@ -127,6 +137,96 @@ async function kcnetTechUpdate(outputPath, apiClient) {
     nLink,
   });
 
+  var newNewsList11 = newData.newsList11;
+  var { nTitle, nDate, nLink } = newNewsList11;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList12 = newData.newsList12;
+  var { nTitle, nDate, nLink } = newNewsList12;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList13 = newData.newsList13;
+  var { nTitle, nDate, nLink } = newNewsList13;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList14 = newData.newsList14;
+  var { nTitle, nDate, nLink } = newNewsList14;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList15 = newData.newsList15;
+  var { nTitle, nDate, nLink } = newNewsList15;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList16 = newData.newsList16;
+  var { nTitle, nDate, nLink } = newNewsList16;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList17 = newData.newsList17;
+  var { nTitle, nDate, nLink } = newNewsList17;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList18 = newData.newsList18;
+  var { nTitle, nDate, nLink } = newNewsList18;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList19 = newData.newsList19;
+  var { nTitle, nDate, nLink } = newNewsList19;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList20 = newData.newsList20;
+  var { nTitle, nDate, nLink } = newNewsList20;
+
+  await apiClient.updateKcnetTech({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
   console.log("kcnetTech updated successfully");
 }
 
@@ -143,16 +243,26 @@ async function kcnetNotUpdate(outputPath, apiClient) {
   const newsCralwer = await KcnetNotCrawling();
 
   const newData = {
-    newsList1: newsCralwer[9],
-    newsList2: newsCralwer[8],
-    newsList3: newsCralwer[7],
-    newsList4: newsCralwer[6],
-    newsList5: newsCralwer[5],
-    newsList6: newsCralwer[4],
-    newsList7: newsCralwer[3],
-    newsList8: newsCralwer[2],
-    newsList9: newsCralwer[1],
-    newsList10: newsCralwer[0],
+    newsList1: newsCralwer[19],
+    newsList2: newsCralwer[18],
+    newsList3: newsCralwer[17],
+    newsList4: newsCralwer[16],
+    newsList5: newsCralwer[15],
+    newsList6: newsCralwer[14],
+    newsList7: newsCralwer[13],
+    newsList8: newsCralwer[12],
+    newsList9: newsCralwer[11],
+    newsList10: newsCralwer[10],
+    newsList11: newsCralwer[9],
+    newsList12: newsCralwer[8],
+    newsList13: newsCralwer[7],
+    newsList14: newsCralwer[6],
+    newsList15: newsCralwer[5],
+    newsList16: newsCralwer[4],
+    newsList17: newsCralwer[3],
+    newsList18: newsCralwer[2],
+    newsList19: newsCralwer[1],
+    newsList20: newsCralwer[0],
   };
 
   // 변경된 값이 없으면 아무 것도 하지 않음
@@ -254,6 +364,96 @@ async function kcnetNotUpdate(outputPath, apiClient) {
     nLink,
   });
 
+  var newNewsList11 = newData.newsList11;
+  var { nTitle, nDate, nLink } = newNewsList11;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList12 = newData.newsList12;
+  var { nTitle, nDate, nLink } = newNewsList12;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList13 = newData.newsList13;
+  var { nTitle, nDate, nLink } = newNewsList13;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList14 = newData.newsList14;
+  var { nTitle, nDate, nLink } = newNewsList14;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList15 = newData.newsList15;
+  var { nTitle, nDate, nLink } = newNewsList15;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList16 = newData.newsList16;
+  var { nTitle, nDate, nLink } = newNewsList16;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList17 = newData.newsList17;
+  var { nTitle, nDate, nLink } = newNewsList17;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList18 = newData.newsList18;
+  var { nTitle, nDate, nLink } = newNewsList18;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList19 = newData.newsList19;
+  var { nTitle, nDate, nLink } = newNewsList19;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList20 = newData.newsList20;
+  var { nTitle, nDate, nLink } = newNewsList20;
+
+  await apiClient.updateKcnetNot({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
   console.log("kcnetNot updated successfully");
 }
 
@@ -270,16 +470,26 @@ async function kcnetNewsUpdate(outputPath, apiClient) {
   const newsCralwer = await KcnetNewsCrawling();
 
   const newData = {
-    newsList1: newsCralwer[9],
-    newsList2: newsCralwer[8],
-    newsList3: newsCralwer[7],
-    newsList4: newsCralwer[6],
-    newsList5: newsCralwer[5],
-    newsList6: newsCralwer[4],
-    newsList7: newsCralwer[3],
-    newsList8: newsCralwer[2],
-    newsList9: newsCralwer[1],
-    newsList10: newsCralwer[0],
+    newsList1: newsCralwer[19],
+    newsList2: newsCralwer[18],
+    newsList3: newsCralwer[17],
+    newsList4: newsCralwer[16],
+    newsList5: newsCralwer[15],
+    newsList6: newsCralwer[14],
+    newsList7: newsCralwer[13],
+    newsList8: newsCralwer[12],
+    newsList9: newsCralwer[11],
+    newsList10: newsCralwer[10],
+    newsList11: newsCralwer[9],
+    newsList12: newsCralwer[8],
+    newsList13: newsCralwer[7],
+    newsList14: newsCralwer[6],
+    newsList15: newsCralwer[5],
+    newsList16: newsCralwer[4],
+    newsList17: newsCralwer[3],
+    newsList18: newsCralwer[2],
+    newsList19: newsCralwer[1],
+    newsList20: newsCralwer[0],
   };
 
   // 변경된 값이 없으면 아무 것도 하지 않음
@@ -374,6 +584,96 @@ async function kcnetNewsUpdate(outputPath, apiClient) {
 
   var newNewsList10 = newData.newsList10;
   var { nTitle, nDate, nLink } = newNewsList10;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList11 = newData.newsList11;
+  var { nTitle, nDate, nLink } = newNewsList11;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList12 = newData.newsList12;
+  var { nTitle, nDate, nLink } = newNewsList12;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList13 = newData.newsList13;
+  var { nTitle, nDate, nLink } = newNewsList13;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList14 = newData.newsList14;
+  var { nTitle, nDate, nLink } = newNewsList14;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList15 = newData.newsList15;
+  var { nTitle, nDate, nLink } = newNewsList15;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList16 = newData.newsList16;
+  var { nTitle, nDate, nLink } = newNewsList16;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList17 = newData.newsList17;
+  var { nTitle, nDate, nLink } = newNewsList17;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList18 = newData.newsList18;
+  var { nTitle, nDate, nLink } = newNewsList18;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList19 = newData.newsList19;
+  var { nTitle, nDate, nLink } = newNewsList19;
+
+  await apiClient.updateKcnetNews({
+    nTitle,
+    nDate,
+    nLink,
+  });
+
+  var newNewsList20 = newData.newsList20;
+  var { nTitle, nDate, nLink } = newNewsList20;
 
   await apiClient.updateKcnetNews({
     nTitle,
